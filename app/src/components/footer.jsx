61c36087c3ffceafa9ec503e46b7db2aaf1d8c94
Eroscodex <karlalondra2003@gmail.com>
2025-03-05 20:57:57 +0800

    first commit

---
+++ app/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className=''>
+      <p className="flex justify-center items-center bg-violet-500">&copy; Karl Alondra</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="flex justify-center items-center bg-violet-500"><b>Philippines</b></h1>
+    </header>
+  );
+};  
+
+export default Header;
